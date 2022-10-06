9140889a0f4df66362b2f452402d2135fe860e28
Shakith <nimsara.shakith@gmail.com>
2022-10-06 12:28:48 +0530

    JSTL Core : remove with scope attribute

---
+++ src/java/com/infinity/webapp/User.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.infinity.webapp;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Shakith
+ */
+public class User implements Serializable {
+    private String name;
+    private String email;
+    private String address;
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+}
